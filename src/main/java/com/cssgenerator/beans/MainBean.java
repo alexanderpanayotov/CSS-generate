c7f6ecfc264d82b2d97ced8fb199a272c3433c61
X <X@Simeon>
2016-12-19 00:05:12 +0200

    Дао-то и конролера са почти готови

--- src/main/java/com/cssgenerator/beans/MainBean.java
+++ src/main/java/com/cssgenerator/beans/MainBean.java
@@ -1,20 +1,26 @@
-package com.cssgenerator.beans;
+packpackage com.cssgenerator.beans;
 
 
 
+import java.util.ArrayList;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.hibernate.HibernateException;
 
+import antlr.collections.List;
 
 import com.cssgenerator.dao.CssStyleDAO;
 import com.cssgenerator.entities.CssStyle;
+import com.cssgenerator.system.JPAUtil;
 
 
 @ManagedBean(name="controller")
@@ -22,38 +28,41 @@ import com.cssgenerator.entities.CssStyle;
 public class MainBean {
 
 	private CssStyle currentStyle = new CssStyle();
+	private ArrayList<CssStyle> listOfCssStyle;
+	private EntityManagerFactory emf = JPAUtil.getEntityManagerFactory();
 	
-	public static void main(String[] args) {
-		save();
-	}
-	
-	public static String save() {
-		EntityManagerFactory emfactory = Persistence
-				.createEntityManagerFactory("Hibernate_JPA");
-		EntityManager entitymanager = emfactory.createEntityManager();
+	public void save() {
+		
+		EntityManager entitymanager = emf.createEntityManager();
+		
 		
 		HttpServletRequest request = (HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest();
-        String css = request.getParameter("preview");
-		 
-
-		CssStyleDAO DAO = new CssStyleDAO();
+		
+		currentStyle.setCss(request.getParameter("css"));
+		currentStyle.setType(request.getParameter("type"));
+		currentStyle.setStyleName(request.getParameter("name"));
+		CssStyleDAO dao = new CssStyleDAO();
 		
 		try {
-//			entitymanager.getTransaction().begin();
-//			DAO.save(getCurrentStyle(), entitymanager);
-//			entitymanager.getTransaction().commit();
+			entitymanager.getTransaction().begin();
+			dao.save(currentStyle, entitymanager);
+			entitymanager.getTransaction().commit();
 
-		} catch (HibernateException e) {
+		} catch (PersistenceException e) {
 			if (entitymanager.getTransaction().isActive()) {
 				entitymanager.getTransaction().rollback();
 			}
+			FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Инфо:", "Неуспешен запис.");
+			FacesContext context = FacesContext.getCurrentInstance();
+			context.addMessage(null, message);
 			e.printStackTrace();
 
 		} finally {
 			entitymanager.close();
-			emfactory.close();
 		}
-		return null;
+	}
+	public void load() {
+		
 	}
 
 	public CssStyle getCurrentStyle() {
@@ -62,6 +71,13 @@ public class MainBean {
 
 	public void setCurrentStyle(CssStyle currentStyle) {
 		this.currentStyle = currentStyle;
+
+	}
+	public ArrayList<CssStyle> getListOfCssStyle() {
+		return listOfCssStyle;
+	}
+	public void setListOfCssStyle(ArrayList<CssStyle> listOfCssStyle) {
+		this.listOfCssStyle = listOfCssStyle;
 	}
 
 }
--- src/main/java/com/cssgenerator/dao/CssStyleDAO.java
+++ src/main/java/com/cssgenerator/dao/CssStyleDAO.java
@@ -2,70 +2,76 @@ package com.cssgenerator.dao;
 
 //import java.util.List;
 
+import java.util.ArrayList;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
 import javax.persistence.Query;
 
+import org.hibernate.HibernateException;
+import org.hibernate.SQLQuery;
 import org.hibernate.event.spi.SaveOrUpdateEvent;
+
+import antlr.collections.List;
 
 import com.cssgenerator.entities.CssStyle;
 
-
 public class CssStyleDAO {
-	
-	public static void save (){//(CssStyle css, EntityManager entitymanager){
-		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Hibernate_JPA" );
-	      EntityManager entitymanager= emfactory.createEntityManager();
-	      Query query = entitymanager.createQuery("SELECT count(*) " + "From CssStyle style");
-		      long count = (Long)query.getSingleResult();
-		      System.out.println(count);
-	   /*   if(count == 1){
-	    	  System.out.println("Error");
-	      }
-	      else{
-	    	  entitymanager.getTransaction( ).begin( );
-	    	  CssStyle style = new CssStyle( ); 
-	    	  long a = 2;
-	         style.setId(a);
-	          style.setStyleName("Css2");
-	          style.setType("bbc");
-	          style.setCss("CSSSsS");
-	          
-	          entitymanager.persist( style );
-	          entitymanager.getTransaction( ).commit( );
 
-	          entitymanager.close( );
-	          emfactory.close( );
-	    	   System.out.println("Gotovo");
-	    	*/      
-	      }
-	//}
-	public boolean load (CssStyle css, EntityManager entitymanager){
-		return false;
+	public void save(CssStyle css, EntityManager entitymanager)
+			throws PersistenceException, IllegalArgumentException {
+
+		Query query = entitymanager
+				.createQuery("SELECT count(*) From CssStyle style where style.type = :type");
+		query.setParameter("type", css.getType());
+		long count = (Long) query.getSingleResult();
+		System.out.println("Брой записи от дадения тип:" + count);
+
+		if (count >= 5) {
+			throw new IllegalArgumentException(
+					"Не е позволено записването на повече от 5 записа!");
+		} else {
+
+			try {
+				entitymanager.persist(css);
+			} catch (PersistenceException e) {
+				e.printStackTrace();
+				throw new PersistenceException(
+						"Грешка в базата при запис на продукт !!!", e);
+			} finally {
+				entitymanager.close();
+			}
+
+		}
+
 	}
-	
-	
-	public static void main(String[] args) {
-		 EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Hibernate_JPA" );
-	      
-	      EntityManager entitymanager = emfactory.createEntityManager( );
-	      entitymanager.getTransaction( ).begin( );
 
-	      CssStyle style = new CssStyle( ); 
-	      style.setId( (long)3);
-	      style.setType( "eqweqwe" );
-	      style.setStyleName("styasdasdle");
-	      style.setCss( "CSSSSS" );
-	      
-	      entitymanager.persist( style );
-	      entitymanager.getTransaction( ).commit( );
-
-	      entitymanager.close( );
-	      emfactory.close( );
+	public ArrayList loadAllStyles(EntityManager entitymanager) {
+		
+		try {
+			String sql = "FROM CssStyle";
+			Query query = entitymanager.createQuery(sql);
+			ArrayList<CssStyle> results = (ArrayList<CssStyle>) query.getResultList();
+			return results;
+		} catch (PersistenceException e) {
+			throw new PersistenceException("Грешка при четене от базата", e);
+		}
 	}
-	
-	
+
+	public ArrayList loadStyleByType(CssStyle css, EntityManager entitymanager) {
+		
+		String type = css.getType();
+		try {
+			String sql = "FROM CssStyle css Where css.type =:type";
+			Query query = entitymanager.createQuery(sql);
+			query.setParameter("type", type);
+			ArrayList<CssStyle> results = (ArrayList<CssStyle>) query.getResultList();
+			return results;
+		} catch (PersistenceException e) {
+			throw new PersistenceException("Грешка при четене от базата", e);
+		}
+	}
 }
-
  
