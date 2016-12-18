5182625843ad56a6209f1bcb9583a811b451ef4c
X <X@Simeon>
2016-12-19 00:33:20 +0200

    Методите save и load в mainBean са имплементирани, но не съм ги тествал

--- src/main/java/com/cssgenerator/beans/MainBean.java
+++ src/main/java/com/cssgenerator/beans/MainBean.java
@@ -1,6 +1,4 @@
-packpackage com.cssgenerator.beans;
-
-
+package com.cssgenerator.beans;
 
 import java.util.ArrayList;
 
@@ -8,6 +6,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -22,47 +21,68 @@ import com.cssgenerator.dao.CssStyleDAO;
 import com.cssgenerator.entities.CssStyle;
 import com.cssgenerator.system.JPAUtil;
 
-
-@ManagedBean(name="controller")
+@ManagedBean(name = "controller")
 @ViewScoped
 public class MainBean {
 
 	private CssStyle currentStyle = new CssStyle();
 	private ArrayList<CssStyle> listOfCssStyle;
 	private EntityManagerFactory emf = JPAUtil.getEntityManagerFactory();
-	
+
 	public void save() {
-		
+
 		EntityManager entitymanager = emf.createEntityManager();
-		
-		
-		HttpServletRequest request = (HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest();
-		
+
+		HttpServletRequest request = (HttpServletRequest) FacesContext
+				.getCurrentInstance().getExternalContext().getRequest();
+
 		currentStyle.setCss(request.getParameter("css"));
 		currentStyle.setType(request.getParameter("type"));
 		currentStyle.setStyleName(request.getParameter("name"));
 		CssStyleDAO dao = new CssStyleDAO();
-		
+
 		try {
 			entitymanager.getTransaction().begin();
 			dao.save(currentStyle, entitymanager);
 			entitymanager.getTransaction().commit();
 
+			FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,
+					"Инфо:", "Успешeн запис.");
+			addFacesContextMessage(message);
+		} catch (IllegalArgumentException e) {
+			FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_WARN,
+					"Инфо:", e.getMessage());
+			addFacesContextMessage(message);
 		} catch (PersistenceException e) {
+
 			if (entitymanager.getTransaction().isActive()) {
 				entitymanager.getTransaction().rollback();
 			}
-			FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Инфо:", "Неуспешен запис.");
-			FacesContext context = FacesContext.getCurrentInstance();
-			context.addMessage(null, message);
+			FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,
+					"Инфо:", "Неуспешен запис.");
+			addFacesContextMessage(message);
 			e.printStackTrace();
 
 		} finally {
 			entitymanager.close();
 		}
 	}
+
 	public void load() {
 		
+		EntityManager entitymanager = emf.createEntityManager();
+		CssStyleDAO dao = new CssStyleDAO();
+		try {
+			listOfCssStyle = dao.loadAllStyles(entitymanager);
+		} catch (PersistenceException e) {
+			e.printStackTrace();
+		}finally{
+			entitymanager.close();
+		}
+	}
+	public void addFacesContextMessage (FacesMessage message){
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.addMessage(null, message);
 	}
 
 	public CssStyle getCurrentStyle() {
@@ -73,9 +93,11 @@ public class MainBean {
 		this.currentStyle = currentStyle;
 
 	}
+
 	public ArrayList<CssStyle> getListOfCssStyle() {
 		return listOfCssStyle;
 	}
+
 	public void setListOfCssStyle(ArrayList<CssStyle> listOfCssStyle) {
 		this.listOfCssStyle = listOfCssStyle;
 	}
